95fb2ef274b695e0b852b3b334bc9e1ece928fff
ericsmslvdr <ericsmsalvador@gmail.com>
2024-06-27 14:45:56 +0800

    Update base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-website/",
+  base: "https://ericsmslvdr.github.io/my-website/",
   plugins: [react()],
   assetsInclude: ['**/*.svg'],
   resolve: {
