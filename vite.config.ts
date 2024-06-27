cd1c9f35732d1cc413a741378d16982b75447a17
ericsmslvdr <ericsmsalvador@gmail.com>
2024-06-27 16:34:34 +0800

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://ericsmslvdr.github.io/my-website/",
+  base: "/",
   plugins: [react()],
   assetsInclude: ['**/*.svg'],
   resolve: {
